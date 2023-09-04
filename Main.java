3369fd91d56d7d6b43a2a41667d8a66d2871308c
navjav90 <128443726+navjav90@users.noreply.github.com>
2023-09-04 17:15:21 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ComputerCase theCase = new ComputerCase("2208","Dell","240");
+
+                Monitor theMonitor =new Monitor("27inch Beast", "Acer", 27,"2540 x 1440");
+        MotherBoard theMotherBoard = new MotherBoard("NJ - 200", "Asus", 4,6,"v2.44");
+
+        PersonelComputer thePC = new   PersonelComputer ("2208", "dell",theCase,theMonitor,theMotherBoard);
+
+//    thePC.getMonitor().drawPixelAt(10,10"red");
+//    thePC.getMotherBoard().loadProgram("Windows OS");
+//    thePC.getComputerCase().pressPowerButton()  ;
+    thePC.powerUp();w
+    }
+}
